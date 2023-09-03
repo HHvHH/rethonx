156c2be8ccc4d63a956122878f4af1fd43824c74
- Haidar <103788745+HHvHH@users.noreply.github.com>
2023-09-03 06:52:20 +0300

    Update checktele.py

--- checktele.py
+++ checktele.py
@@ -212,6 +212,20 @@ def gen_user(choice):
             username = ''.join(f)
         else:
             pass
+            if choice == "14":
+        c = d = random.choices(a)
+        d = random.choices(e)
+        f =  [c[0], d[0], d[0], d[0], c[0]]
+        random.shuffle(f)
+        username = ''.join(f)
+        if username in banned[0]:
+            c = d = random.choices(a)
+            d = random.choices(b)
+            f =  [c[0], d[0], d[0], d[0], c[0]]
+            random.shuffle(f)
+            username = ''.join(f)
+        else:
+            pass
     return username
 
 @sython.on(events.NewMessage(outgoing=True, pattern=r"\.تشيكر"))
